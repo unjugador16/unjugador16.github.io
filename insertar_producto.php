342b95a7a3e10306a35b7e7128093b5e8342f4d3
unjugador16 <103539827+unjugador16@users.noreply.github.com>
2024-06-03 22:57:34 -0500

    Update insertar_producto.php

--- insertar_producto.php
+++ insertar_producto.php
@@ -1,13 +1,16 @@
 <?php 
 
-include 'conexion.php';
-/*
-$codigo = $_POST["codigo"];
-$nombre = $_POST["nombre"];
-$precio = $_POST["precio"];
-$stock = $_POST["stock"];
+$hostname = "b0w2sdcfmwli4czllxje-mysql.services.clever-cloud.com"; // bg0bgu95i1hddoqybtyd-mysql.services.clever-cloud.com
+$database = "b0w2sdcfmwli4czllxje"; // bg0bgu95i1hddoqybtyd
+$username = "u67qvvog4xmmiwfi"; // u6opw9qrerzhn3xy
+$password = "ptXsJd2O1apwmKz41ycc"; // O7w1EnAq8ou6RjiUKsIA
 
-$consulta = "INSERT INTO productos (codigo, nombre, precio, stock) VALUES ('$codigo','$nombre','$precio','$stock')"; */
+
+$conexion = new mysqli($hostname, $username, $password, $database);
+
+if ($conexion -> connect_errno) {
+    echo "lo sentimos, el sitio web está experimentando problemas";
+}
 
 $names = $_POST["names"];
 $surnames = $_POST["surnames"];
